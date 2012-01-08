fe964720f5c0fba5b9cadc0d104f970c82810b4b
Tatiana Merkulova <tatianamr@ya.ru>
2012-01-09 01:17:31 +0300

    ConnectionHandler class is partially implemented.

--- src/main/java/ru/hh/school/stdlib/ConnectionHandler.java
+++ src/main/java/ru/hh/school/stdlib/ConnectionHandler.java
@@ -1,11 +1,38 @@
 package ru.hh.school.stdlib;
 
+import java.io.*;
+import java.net.Socket;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Tiana
  * Date: 09.01.12
  * Time: 0:08
  */
-public class ConnectionHandler {
+public class ConnectionHandler implements Runnable {
+    protected Socket socket;
 
+    public ConnectionHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        final Writer output;
+        final Reader input;
+
+        try{
+            output = new OutputStreamWriter(socket.getOutputStream());
+            input = new InputStreamReader(socket.getInputStream());
+        } catch (IOException ex) {
+            System.err.println("Unable to get input or output stream of the given socket.");
+            return;
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException ignored) {
+            }
+        }
+
+        // todo to implement the handling.
+    }
 }
