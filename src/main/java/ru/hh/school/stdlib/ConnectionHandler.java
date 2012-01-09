662d49d7ffbbb4b6aadfe275842b7ef275a80a76
Tatiana Merkulova <tatianamr@ya.ru>
2012-01-10 00:20:09 +0300

    Run method in ConnectionHandler class is partially written.

--- src/main/java/ru/hh/school/stdlib/ConnectionHandler.java
+++ src/main/java/ru/hh/school/stdlib/ConnectionHandler.java
@@ -2,6 +2,7 @@ package ru.hh.school.stdlib;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.StringTokenizer;
 
 /**
  * Created by IntelliJ IDEA.
@@ -10,29 +11,91 @@ import java.net.Socket;
  * Time: 0:08
  */
 public class ConnectionHandler implements Runnable {
-    protected Socket socket;
+    protected final Socket socket;
+    protected final Substitutor3000 substitutor;
 
-    public ConnectionHandler(Socket socket) {
+    public ConnectionHandler(final Socket socket, final Substitutor3000 substitutor) {
         this.socket = socket;
+        this.substitutor = substitutor;
     }
 
     public void run() {
         final Writer output;
-        final Reader input;
+        final BufferedReader input;
 
-        try{
-            output = new OutputStreamWriter(socket.getOutputStream());
-            input = new InputStreamReader(socket.getInputStream());
-        } catch (IOException ex) {
-            System.err.println("Unable to get input or output stream of the given socket.");
-            return;
+        try {
+            try {
+                output = new OutputStreamWriter(socket.getOutputStream());
+                input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            } catch (IOException ex) {
+                System.err.println("Unable to get input or output stream of the given socket.");
+                return;
+            }
+
+            String currentLine;
+            try{
+                currentLine = input.readLine();
+            } catch (IOException ex) {
+                System.err.println("Unable to read from input stream of the given socket.");
+                return;
+            }
+
+            StringTokenizer parser = new StringTokenizer(currentLine);
+
+            try{
+                if (parser.hasMoreTokens()) {
+                    String commandName = parser.nextToken();
+                    if (commandName.equals("GET")) {
+                        if (parser.hasMoreTokens()) {
+                            String key = parser.nextToken();
+                            String getAnswer = substitutor.get(key);
+                            if (getAnswer.equals("") && parser.hasMoreTokens()) {
+                                String defaultValue = parser.nextToken();
+                                substitutor.put(key, defaultValue);
+                            }
+                            output.write("VALUE");
+                            output.write(getAnswer);
+                        } else {
+                            System.err.println("Command GET needs a key.");
+                            return;
+                        }
+                    } else if (commandName.equals("PUT")) {
+                        if (parser.hasMoreTokens()) {
+                            String key = parser.nextToken();
+                            if (parser.hasMoreTokens()) {
+                                String value = parser.nextToken();
+                                substitutor.put(key, value);
+                                output.write("OK");
+                            } else {
+                                System.err.println("Command PUT needs value for key " + key + ".");
+                                return;
+                            }
+                        } else {
+                            System.err.println("Command PUT needs arguments.");
+                            return;
+                        }
+
+                    } else if (commandName.equals("SET")) {
+
+                    } else {
+                        System.err.println("Command " + commandName + " doesn't exist.");
+                        return;
+                    }
+                } else {
+                    System.err.println("Empty input string.");
+                    return;
+                }
+            } catch (IOException ex) {
+                System.err.println("Unable to write to output stream of socket.");
+                return;
+            }
+            
+            // todo to implement the handling.
         } finally {
             try {
                 socket.close();
             } catch (IOException ignored) {
             }
         }
-
-        // todo to implement the handling.
     }
 }
