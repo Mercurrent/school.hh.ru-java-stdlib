a219da0eff8309907a6929f91333b4c6440ce6db
Tatiana Merkulova <tatianamr@ya.ru>
2012-01-10 14:40:45 +0300

    Put method in Substitutor3000 is written; transfromValueString method in Substitutor3000 is partially written;

--- src/main/java/ru/hh/school/stdlib/Substitutor3000.java
+++ src/main/java/ru/hh/school/stdlib/Substitutor3000.java
@@ -1,11 +1,42 @@
 package ru.hh.school.stdlib;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Substitutor3000 {
-    public void put(String key, String value) {
-        throw new UnsupportedOperationException();
+    private final Map<String, String> internalMap = new HashMap<String, String>();
+
+    protected String transformValueString(String valueString) {
+        int fromIndex = 0;
+
+        final StringBuilder result = new StringBuilder();
+        
+        int openingBraceIndex = valueString.indexOf("${", fromIndex);
+        while (openingBraceIndex != -1) {
+            result.append(valueString, fromIndex, openingBraceIndex);
+            int closingBraceIndex = valueString.indexOf("}", fromIndex);
+            if (closingBraceIndex != -1) {
+                result.append(internalMap.get(valueString.substring(openingBraceIndex + 2, closingBraceIndex)));
+                fromIndex = closingBraceIndex + 1;
+            } else {
+                result.append(valueString.substring(openingBraceIndex));
+                fromIndex = valueString.length() - 1;
+            }
+            openingBraceIndex = valueString.indexOf("${", fromIndex);
+        }
+        result.append(valueString, fromIndex, valueString.length());
+
+        return result.toString();
+    }
+    
+    public synchronized void put(String key, String value) {
+        internalMap.put(key, value);
     }
 
-    public String get(String key) {
-        throw new UnsupportedOperationException();
+    public synchronized String get(String key) {
+        String valueString = internalMap.get(key);
+        String resultString = transformValueString(valueString);
+        //todo to write get
     }
 }
